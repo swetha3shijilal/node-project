26b9f4b6f39fc8e65f7632912fd3cc645814737e
swetha k s <swethakshijilal@gmail.com>
2025-06-11 14:17:02 +0530

    message

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port=3001;
+app.use(cors());
+app.use(express.json());
+app.get('/getdata/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.send("hello world");
+});
+app.post('/createdata',(req,res)=>{
+    const data =req.body;
+    console.log(data);
+    res.send("data recieved");
+});
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log("server running on port",{port})
+})
